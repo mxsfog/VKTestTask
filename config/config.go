d7161639ee958f45d15439c1184502083d153ac0
mxsfog <mxsfog@gmail.com>
2024-07-12 15:58:57 +0300

    added config for bd

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		DBName   string `yaml:"dbname"`
+		Port     int    `yaml:"port"`
+		SSLMode  string `yaml:"sslmode"`
+		TimeZone string `yaml:"timezone"`
+	} `yaml:"database"`
+	Kafka struct {
+		Brokers        []string `yaml:"brokers"`
+		Topic          string   `yaml:"topic"`
+		ProcessedTopic string   `yaml:"processed_topic"`
+	} `yaml:"kafka"`
+}
+
+func loadConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
